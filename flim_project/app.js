0b427e134547bc92fed4159dfea0932b0fb846aa
minhtk1 <43837476+minhtk1@users.noreply.github.com>
2019-01-03 22:59:22 +0700

    Add files via upload

---
+++ flim_project/app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+var cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  
+  app.set("views", path.join(__dirname, "views"));
+  app.engine('ejs', engine);
+  app.set("view engine", "ejs");
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Express session
+  app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+  // Connect flash
+  app.use(flash());
+
+  // Global variables
+  app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+// Routes
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err)
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: err
+      });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+      message: err.message,
+      error: {}
+  });
+});
+
+module.exports = app;
+  
